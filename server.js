da4902e6778a22b9efdb322220f1349c3597334d
Ruchika Muddinagiri <rrmuddinagiri@gmail.com>
2020-10-04 22:08:28 +0530

    .

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(cookieParser())
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, './views'));
 app.use(session({
-    secret: process.env.SECRET,
+    secret: "shhhhhhhhhhhhhhhhhhhhhhhkoihai",
     resave: false,
     saveUninitialized: true,
     store: sessionStore,
