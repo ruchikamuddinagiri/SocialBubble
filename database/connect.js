8329ffcf3f557cc191e1749fd2b157fef11c3f98
Ruchika Muddinagiri <rrmuddinagiri@gmail.com>
2020-10-06 00:22:09 +0530

    changes

--- database/connect.js
+++ database/connect.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const dbString = 'mongodb://127.0.0.1:27017/socialbubble'
+
+const dbString = process.env.DATABASE_URI || 'mongodb://127.0.0.1:27017/socialbubble'
 const db = () => {
   console.log(dbString)
   return mongoose
